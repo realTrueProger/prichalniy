4957e30d4732583ac94f503beb67b891e2fd2de5
Vladimir Solovev <vsolovev@inno.tech>
2024-05-17 16:52:56 +0300

    refactor

--- src/Initiatives.js
+++ src/Initiatives.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {Grid} from "@mui/material";
+import {Box} from "@mui/material";
 
 function Initiatives() {
     return (
-        <Grid container>
-            <Grid item>
-                <h1>Ссылка на регламенты</h1>
-                <a target="_blank"
-                   href={"https://drive.google.com/drive/folders/1gga8KH2QIBom1z9vowjZYSdLSBW5EuQF?usp=sharing"}>Регламенты</a>
-            </Grid>
-        </Grid>
+        <Box>
+            <h1>Ссылка на регламенты</h1>
+            <a target="_blank"
+               href={"https://drive.google.com/drive/folders/1gga8KH2QIBom1z9vowjZYSdLSBW5EuQF?usp=sharing"}>Регламенты</a>
+        </Box>
     );
 }
 
