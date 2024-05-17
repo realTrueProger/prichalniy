38593f09fa36ab838497875edf2631cced4e97db
Vladimir Solovev <vsolovev@inno.tech>
2024-05-17 16:44:58 +0300

    applu container

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import Home from "./Home";
 import Initiatives from "./Initiatives";
 import Oss from "./Oss";
-import {Grid} from "@mui/material";
+import {Box, Container, Grid} from "@mui/material";
 
 const baseName = process.env.NODE_ENV === 'production' ? "/prichalniy/" : "/";
 
 function App() {
     return (
         <Router basename={baseName}>
-            <div className="App">
+            <Container >
                 <Grid container justifyContent="space-between" spacing={2} className="App-header">
                     <Grid item>
-                        <div className="logo">
+                        <Box className="logo">
                             <div>Инициативная группа</div>
                             <div>Level Причальный</div>
-                        </div>
+                        </Box>
                     </Grid>
+
                     <Grid item>
-                        <nav>
-                            <ul>
-                                <li><Link to="/">Главная</Link></li>
-                                <li>|</li>
-                                <li><Link to="/initiatives">Регламенты</Link></li>
-                                <li>|</li>
-                                <li><Link to="/oss">ОСС</Link></li>
-                            </ul>
-                        </nav>
+                        <Box>
+                            <nav>
+                                <ul>
+                                    <li><Link to="/">Главная</Link></li>
+                                    <li>|</li>
+                                    <li><Link to="/initiatives">Регламенты</Link></li>
+                                    <li>|</li>
+                                    <li><Link to="/oss">ОСС</Link></li>
+                                </ul>
+                            </nav>
+                        </Box>
                     </Grid>
                 </Grid>
 
@@ -38,7 +41,7 @@ function App() {
                         <Route path="/oss" element={<Oss/>}/>
                     </Routes>
                 </main>
-            </div>
+            </Container>
         </Router>
     );
 }
