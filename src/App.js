017ebb26d644a6a5cf8f7b7c56e1526504fca349
Vladimir Solovev <vsolovev@inno.tech>
2024-05-17 12:06:33 +0300

    site v2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Home";
+import Initiatives from "./Initiatives";
+import Oss from "./Oss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className="App">
+              <header className="App-header">
+                  <div className="logo">
+                      <div>Инициативная группа</div>
+                      <div>Level Причальный</div>
+                  </div>
+                  <nav>
+                      <ul>
+                          <li><Link to="/">Главная</Link></li>
+                          <li><Link to="/initiatives">Инициативы</Link></li>
+                          <li><Link to="/oss">ОСС</Link></li>
+                      </ul>
+                  </nav>
+              </header>
+              <main>
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/initiatives" element={<Initiatives />} />
+                      <Route path="/oss" element={<Oss />} />
+                  </Routes>
+              </main>
+          </div>
+      </Router>
   );
 }
 
---
+++ src/Initiatives.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Initiatives() {
+    return (
+        <div className="content">
+            <div className="text">
+                <h1>Наши предложения</h1>
+                <p> Список предложение пуст</p>
+            </div>
+        </div>
+    );
+}
+
+export default Initiatives;
