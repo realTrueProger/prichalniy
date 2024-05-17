8c0adb7de8cf1ae9edacc85da3745ef3deb15cbe
Vladimir Solovev <vsolovev@inno.tech>
2024-05-17 12:51:43 +0300

    clear old config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,7 +11,8 @@ import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <CssBaseline/>
+        <App/>
+    </React.StrictMode>
 );
